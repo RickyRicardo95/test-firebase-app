4d1408ecd14c32016b52db0822e6f6099f810062
rwreyesbe <rwreyesbe@gmail.com>
2025-04-16 02:53:19 +0000

    initial commit of firebase initialize

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { WINDOW, windowProvider } from './providers/window';
+import { DOCUMENT } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideRouter(routes),
+    provideClientHydration(),
+    {
+      provide: WINDOW,
+      useFactory: (document: Document) => windowProvider(document),
+      deps: [DOCUMENT],
+    },
+  ]
 };
