ca8372d0c8045371fcf657ba2df12832353a37c4
rwreyesbe <rwreyesbe@gmail.com>
2025-04-22 00:54:36 +0000

    deleted boiler plate code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,18 +6,6 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)
   },
   {
-    path: 'deferrable-views',
-    loadComponent: () => import('./pages/deferrable-views/deferrable-views.component').then(c => c.DeferrableViewsComponent),
-  },
-  {
-    path: 'ssg',
-    loadComponent: () => import('./pages/ssg/ssg.component').then(c => c.SsgComponent)
-  },
-  {
-    path: 'ssr',
-    loadComponent: () => import('./pages/ssr/ssr.component').then(c => c.SsrComponent)
-  },
-  {
     path: '**',
     loadComponent: () => import('./pages/not-found/not-found.component').then(c => c.NotFoundComponent)
   }
